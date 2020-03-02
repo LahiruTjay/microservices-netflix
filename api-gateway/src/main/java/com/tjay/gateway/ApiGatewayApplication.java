c9aa8fe35691f75794d73c4b692c06296924e363
LahiruTjay <lahiru.tjay@gmail.com>
2020-03-02 19:34:34 +0530

    Commit: Hystrix fallback added for API gateway

--- api-gateway/src/main/java/com/tjay/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/tjay/gateway/ApiGatewayApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- api-gateway/src/main/java/com/tjay/gateway/controller/UserAPI.java
+++ api-gateway/src/main/java/com/tjay/gateway/controller/UserAPI.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class UserAPI {
 
@@ -12,9 +14,14 @@ public class UserAPI {
 	private RestTemplate restTemplate;
 
 	@GetMapping("/getUser")
+	@HystrixCommand(fallbackMethod = "getUserFallback")
 	public String getUser() {
 		String url = "http://user-service/user";
 		return restTemplate.getForObject(url, String.class);
 	}
 
+	public String getUserFallback() {
+		return "User-Service";
+	}
+	
 }
